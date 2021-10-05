234cdff1d5ecca2e79922b2295159b664c51c0e5
Gabriel Negrelli <gnegrelli13@gmail.com>
2021-10-05 20:25:46 -0300

    Create models for users and tweets

--- tracker/models.py
+++ tracker/models.py
@@ -1,9 +1,27 @@
 from django.db import models
 
 
-class Tweet(models.Model):
-    """Tweets fetch from Twitter API"""
-
-
 class TwitterUser(models.Model):
     """Information from Twitter user"""
+    twitter_id = models.CharField(max_length=20)
+    username = models.CharField(max_length=120)
+    profile_name = models.CharField(max_length=120, null=True, blank=True)
+    followers = models.IntegerField(null=True, blank=True)
+    verified = models.BooleanField(default=False)
+    joined_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Tweet(models.Model):
+    """Tweets retrieved from Twitter API"""
+    tweet_id = models.CharField(max_length=40)
+    user_id = models.ForeignKey(TwitterUser, on_delete=models.CASCADE, related_name='tweets')
+    content = models.CharField(max_length=400)
+    relevant_tokens = models.CharField(max_length=400)
+    tweeted_at = models.DateTimeField(null=True, blank=True)
+    deleted = models.BooleanField(default=False)
+    likes = models.IntegerField(null=True, blank=True)
+    retweets = models.IntegerField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
