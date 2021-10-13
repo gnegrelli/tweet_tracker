fc5f81805b533d78c691b26d532e4e1fc496f97f
Gabriel Negrelli <gnegrelli13@gmail.com>
2021-10-13 20:13:48 -0300

    Rename model field and set str values

--- tracker/models.py
+++ tracker/models.py
@@ -12,11 +12,14 @@ class TwitterUser(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'{self.profile_name} (@{self.username})'
+
 
 class Tweet(models.Model):
     """Tweets retrieved from Twitter API"""
     tweet_id = models.CharField(max_length=40)
-    user_id = models.ForeignKey(TwitterUser, on_delete=models.CASCADE, related_name='tweets')
+    user = models.ForeignKey(TwitterUser, on_delete=models.CASCADE, related_name='tweets')
     content = models.CharField(max_length=400)
     relevant_tokens = models.CharField(max_length=400)
     tweeted_at = models.DateTimeField(null=True, blank=True)
@@ -25,3 +28,8 @@ class Tweet(models.Model):
     retweets = models.IntegerField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        max_len = 40
+        ending = '...' if len(self.content) > max_len else ''
+        return f'{self.user!s}: \"{self.content[:max_len]}{ending}\"'
