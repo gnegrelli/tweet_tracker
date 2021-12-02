e137817f94c97c212c421e6a4a55f8809b21cb9a
Gabriel Negrelli <gnegrelli13@gmail.com>
2021-12-02 08:46:47 -0300

    Create classes for admin page

--- tracker/admin.py
+++ tracker/admin.py
@@ -3,5 +3,24 @@ from django.contrib import admin
 from tracker.models import Tweet, TwitterUser
 
 
-admin.site.register(TwitterUser)
-admin.site.register(Tweet)
+@admin.register(Tweet)
+class TweetAdmin(admin.ModelAdmin):
+    list_display = (
+        'tweet_id',
+        '__str__',
+        'tweeted_at',
+        'likes',
+        'retweets',
+        'deleted'
+    )
+
+
+@admin.register(TwitterUser)
+class TwitterUserAdmin(admin.ModelAdmin):
+    list_display = (
+        'twitter_id',
+        '__str__',
+        'followers',
+        'verified',
+        'joined_at'
+    )
