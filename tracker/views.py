a25520bb9bf4cb6b8f92a3ed22fb4a953b5d2ced
Gabriel Negrelli <gnegrelli13@gmail.com>
2022-01-21 22:25:18 -0300

    Create get view

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .exceptions import UnknownUser
+from .services import user_wordcloud
+
+
+class GetTweetsView(APIView):
+    class Meta:
+        pass
+
+    def get(self, request, *args, **kwargs):
+        username = kwargs.get('username')
+        try:
+            tokens = user_wordcloud(username)
+        except UnknownUser:
+            return Response({'error': f'Unknown user: {username}'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'username': username, 'tokens': tokens}, status=status.HTTP_200_OK)
