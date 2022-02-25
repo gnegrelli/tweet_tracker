6c7d41a927bc25adb5174aa4d5b502f03a1cb761
Gabriel Negrelli <gnegrelli13@gmail.com>
2022-02-25 19:36:14 -0300

    Add/upadte users when backend starts

--- tracker/apps.py
+++ tracker/apps.py
@@ -1,6 +1,15 @@
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class TrackerConfig(AppConfig):
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tracker'
+
+    def ready(self):
+        """Add/Update users every time backend starts"""
+
+        from .services import add_twitter_users
+
+        add_twitter_users(settings.USERS)
